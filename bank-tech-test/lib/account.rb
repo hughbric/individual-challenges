d940a4031cf3b3a493f911f56ca2be539762174d
Michael Hughes <mhughes36@live.co.uk>
2019-07-01 14:05:40 +0100

    account has a balance

--- bank-tech-test/lib/account.rb
+++ bank-tech-test/lib/account.rb
@@ -1,3 +1,9 @@
 class Account
+  def initialize(opening_balance)
+    @balance = opening_balance
+  end
 
+  def balance
+    @balance
+  end
 end
--- bank-tech-test/spec/account_spec.rb
+++ bank-tech-test/spec/account_spec.rb
@@ -1,5 +1,8 @@
 require 'account'
 
 describe Account do
-  
+  it 'has a balance' do
+    account = Account.new(2000)
+    expect(account.balance).to eq(2000)
+  end
 end
