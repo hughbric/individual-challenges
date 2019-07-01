0be7d81fc55c10a083649dab589fafd6aeeb4cfc
Michael Hughes <mhughes36@live.co.uk>
2019-07-01 14:18:15 +0100

    withdraw method

--- bank-tech-test/spec/account_spec.rb
+++ bank-tech-test/spec/account_spec.rb
@@ -1,13 +1,17 @@
 require 'account'
 
 describe Account do
-  subject { described_class.new(2000) }
+  subject { described_class.new(1000) }
 
   it 'has a balance' do
-    expect(subject.balance).to eq(2000)
+    expect(subject.balance).to eq(1000)
   end
 
   it 'can be depositted' do
-    expect(subject.deposit(500)).to eq(2500)
+    expect(subject.deposit(2000)).to eq(3000)
+  end
+
+  it 'can be withdrawn' do
+    expect(subject.withdraw(500)).to eq(500)
   end
 end
