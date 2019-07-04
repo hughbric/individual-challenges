27d8ae451a38b701e28ed54a3156d5c425cae3fb
Michael Hughes <mhughes36@live.co.uk>
2019-07-04 13:47:03 +0100

    board setup

--- tic-tac-toe/lib/game.rb
+++ tic-tac-toe/lib/game.rb
@@ -1,3 +1,6 @@
 class Game
+  def initialize
+    puts "1|2|3\n4|5|6\n7|8|9\n"
+  end
 
 end
--- tic-tac-toe/spec/game_spec.rb
+++ tic-tac-toe/spec/game_spec.rb
@@ -1,5 +1,7 @@
 require 'game'
 
 describe Game do
-
+  it 'board is setup' do
+    expect { Game.new }.to output("1|2|3\n4|5|6\n7|8|9\n").to_stdout
+  end
 end
