28258f66aeed5b3b16ee2e2f5c01838d53bd4e57
Michael Hughes <mhughes36@live.co.uk>
2019-07-04 14:04:55 +0100

    first move: marks an X

--- tic-tac-toe/lib/game.rb
+++ tic-tac-toe/lib/game.rb
@@ -1,6 +1,17 @@
 class Game
   def initialize
-    puts "1|2|3\n4|5|6\n7|8|9\n"
+    @board = "1|2|3\n4|5|6\n7|8|9\n"
+    puts @board
   end
 
+  def move(position)
+    board_update = @board.chars.map! { |item|
+      if item == position.to_s
+        item = 'X'
+      else
+        item
+      end
+      }
+    puts board_update.join
+  end
 end
--- tic-tac-toe/spec/game_spec.rb
+++ tic-tac-toe/spec/game_spec.rb
@@ -4,4 +4,9 @@ describe Game do
   it 'board is setup' do
     expect { Game.new }.to output("1|2|3\n4|5|6\n7|8|9\n").to_stdout
   end
+
+  it 'receives input and displays X on board' do
+    game = Game.new
+    expect { game.move(1) }.to output("X|2|3\n4|5|6\n7|8|9\n").to_stdout
+  end
 end
